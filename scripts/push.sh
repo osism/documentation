08373897a4014eb9eb0770acf696951b01d446f0
Christian Berendt <berendt@betacloud-solutions.de>
2018-08-01 13:29:24 +0200

    Do not remove build/html/_sources

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rm -rf build/html/.buildinfo build/html/.doctrees build/html/_sources
+rm -rf build/html/.buildinfo build/html/.doctrees
 lftp -u "$FTP_USERNAME,$FTP_PASSWORD" -e "set ftp:ssl-allow no; mirror -R --delete build/html osism; exit;" ftp://betacloud.io
