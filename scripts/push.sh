bbb6ebe365580b9cc5787d1764f77510a96bb015
Christian Berendt <berendt@betacloud-solutions.de>
2018-12-19 10:33:36 +0100

    Improve push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf build/html/.buildinfo build/html/.doctrees
-lftp -u "$FTP_USERNAME,$FTP_PASSWORD" -e "set ftp:ssl-allow no; mirror -R --delete build/html osism; exit;" ftp://betacloud.io
+lftp -u "$FTP_USERNAME,$FTP_PASSWORD" -e "set set ssl:verify-certificate false; mirror -v -R --delete build/html osism; exit;" ftp://betacloud.io
