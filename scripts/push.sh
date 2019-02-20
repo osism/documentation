7ca9a0f8a61a9363cacf2f30c457d23c7e47d7fd
Christian Berendt <berendt@betacloud-solutions.de>
2019-02-20 23:55:59 +0100

    Remove build scripts

--- scripts/push.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf build/html/.buildinfo build/html/.doctrees
-lftp -u "$FTP_USERNAME,$FTP_PASSWORD" -e "set ssl:verify-certificate false; mirror -v -R --delete build/html osism; exit;" ftp://betacloud.io
