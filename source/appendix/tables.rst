efa167ee76edc55091468121a0261b4243397d2b
Christian Berendt <berendt@osism.tech>
2022-04-19 21:30:50 +0200

    appendix: remove ansible roles & docker images
    The individual repositories no longer exist in this form.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- source/appendix/tables.rst
+++ source/appendix/tables.rst
@@ -5,8 +5,6 @@ Tables
 .. toctree::
    :maxdepth: 2
 
-   tables/ansible-roles
-   tables/docker-images
    tables/networks
    tables/node-types
    tables/releases
--- src/gh-docker-images.py
+++
@@ -1,6 +0,0 @@
-import github
- 
-gh = github.Github()
- 
-for repository in [x for x in gh.get_organization("osism").get_repos() if x.full_name.startswith("osism/docker") and not x.archived and not "template" in x.full_name and "kolla-docker" not in x.full_name]:
-    print("%s %s osism/%s" % (repository.full_name.ljust(40), repository.html_url.ljust(60), repository.full_name[13:]))
